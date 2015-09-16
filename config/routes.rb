dbd9ebe95c157440242f3050e2ff6ca115b14792
Craig Buchanan <spoonerst@gmail.com>
2015-09-16 20:22:08 +0000

    Added pages controller, home and about view, and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  # About address points to "pages" controller and the "about" action.
+  get '/about' => 'pages#about'
+  # Home page needs to route to "pages" controller and the "home" action.
+  root 'pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
