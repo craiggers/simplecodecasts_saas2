10e1d89b6c804524b6d2a4bbb6ce5f350ba551e9
Craig Buchanan <spoonerst@gmail.com>
2015-09-20 02:30:44 +0000

    Added validations and create actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,8 +1,27 @@
 class ContactsController < ApplicationController
   def new
+    # Store empty Contact object to help generate Form
     @contact = Contact.new
   end
   
   def create
+    @contact = Contact.new(contact_params)
+    # When we save the contacts
+    if @contact.save
+      # Show Flash notice
+      flash[:success] = 'Message sent.'
+      # Redirect to the same new contact url
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Error occured, message has not been sent.'
+      redirect_to new_contact_path
+    end
   end
+  
+  private
+    def contact_params
+      # White list the parameters
+      params.require(:contact).permit(:name, :email, :comments)
+    end
+  
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ActiveRecord::Base
+    # Active record validations
+    validates :name, presence: true
+    validates :email, presence: true
 end
