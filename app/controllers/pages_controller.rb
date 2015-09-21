56cfbb7479b6e45a4e6ce50b0d1f728cbb8cc71f
Craig Buchanan <spoonerst@gmail.com>
2015-09-21 03:14:17 +0000

    Added plan variables and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
     def home
+        # Define instance variables assigned to Database tables
+        # Find method "plucks out" Plan with particular ID
+        @basic_plan = Plan.find(1)
+        @pro_plan = Plan.find(2)
     end
     
     def about
