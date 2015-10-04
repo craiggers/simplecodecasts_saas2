2ac48e9ce767e13b5eec17da38b36e1ad4a858fd
Craig Buchanan <spoonerst@gmail.com>
2015-10-04 02:54:40 +0000

    Added index action and updated link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   # Before any Action, user must be authenticated (exist/signed in)
   before_action :authenticate_user!
   
+  def index
+  end
+  
   def show
     # Find User in database by their ID.
     @user = User.find( params[:id] )
